79a32175a33571f06998a4e409d3caa84d153d1f
Patrick Dillon <padillon@redhat.com>
2024-12-13 10:26:58 -0500

    Add marketplace extension
    Adds a marketplace extension to contain cloud marketplace offerings.
    Initially it only contains unpaid Azure marketplace images, but could
    potentially include images for AWS, GCP, and paid Azure.

--- stream/rhcos/rhcos.go
+++ stream/rhcos/rhcos.go
@@ -2,7 +2,8 @@ package rhcos
 
 // Extensions is data specific to Red Hat Enterprise Linux CoreOS
 type Extensions struct {
-	AzureDisk *AzureDisk `json:"azure-disk,omitempty"`
+	AzureDisk   *AzureDisk   `json:"azure-disk,omitempty"`
+	Marketplace *Marketplace `json:"marketplace,omitempty"`
 }
 
 // AzureDisk represents an Azure disk image that can be imported
@@ -15,4 +16,26 @@ type AzureDisk struct {
 	// that can be copied into an image gallery.  Avoid creating VMs directly
 	// from this URL as that may lead to performance limitations.
 	URL string `json:"url,omitempty"`
+}
+
+// Marketplace contains marketplace images for all clouds.
+type Marketplace struct {
+	Azure *AzureMarketplace `json:"azure,omitempty"`
+}
+
+// AzureMarketplaceImage defines the attributes for an Azure
+// marketplace image.
+type AzureMarketplaceImage struct {
+	Publisher       string
+	Offer           string
+	SKU             string
+	Version         string
+	ThirdPartyImage bool
+}
+
+// AzureMarketplace lists images, both paid and
+// unpaid, available in the Azure marketplace.
+type AzureMarketplace struct {
+	// NoPurchasePlan is the standard, unpaid RHCOS image.
+	NoPurchasePlan *AzureMarketplaceImage `json:"no-purchase-plan,omitempty"`
 }
